0b0579204a7a1c2fd0f0a39b718d27b09254daec
Lotfi <lotfi.doucanef@pertimm.com>
2021-04-13 15:43:30 +0200

    [ADD] stored resources and component to add resourses had been added

---
+++ src/components/StoredResources.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul>
+        <learning-resource v-for="learningResource in resources" :key="learningResource.id"
+            :title="learningResource.title" 
+            :description="learningResource.description" 
+            :link="learningResource.link">
+        </learning-resource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource';
+
+export default {
+    inject: ['resources'],
+    components:{
+        LearningResource
+        },
+    data() {
+
+    },
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
